a4f18e5e4df1563c5dd925706a71ace666aab5c4
dzwduan <dzwduan@gmail.com>
2022-04-01 00:51:15 -0700

    Update Lab1 fpga version

--- example/csrc/main.cpp
+++ example/csrc/main.cpp
@@ -2,13 +2,14 @@
 #include "verilated_vcd_c.h"
 #include <Vtop.h>
 
-
+static Vtop dut;
 void nvboard_bind_all_pins(Vtop* top);
 
-// static void single_cycle() {
-//   dut.clk = 0; dut.eval();
-//   dut.clk = 1; dut.eval();
-// }
+static void single_cycle() {
+  // dut.clk = 0; dut.eval();
+  // dut.clk = 1;
+  dut.eval();
+}
 
 // static void reset(int n) {
 //   dut.rst = 1;
@@ -16,62 +17,60 @@ void nvboard_bind_all_pins(Vtop* top);
 //   dut.rst = 0;
 // }
 
-// int main() {
-//   nvboard_bind_all_pins(&dut);
-//   nvboard_init();
+int main() {
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
 
-//   reset(10);
+  while(1) {
+    nvboard_update();
+    single_cycle();
+  }
+}
 
-//   while(1) {
-//     nvboard_update();
-//     single_cycle();
-//   }
+// VerilatedContext* contextp = NULL;
+// VerilatedVcdC* tfp = NULL;
+
+// static Vtop* top;
+
+// void step_and_dump_wave(){
+//   top->eval();
+//   nvboard_update();
+//   contextp->timeInc(1);
+//   tfp->dump(contextp->time());
 // }
 
-VerilatedContext* contextp = NULL;
-VerilatedVcdC* tfp = NULL;
 
-static Vtop* top;
+// void sim_init(){
+//   contextp = new VerilatedContext;
+//   tfp = new VerilatedVcdC;
+//   top = new Vtop;
+//   nvboard_init();
+//   contextp->traceEverOn(true);
+//   top->trace(tfp, 0);
+//   tfp->open("dump.vcd");
+// }
 
-void step_and_dump_wave(){
-  top->eval();
-  nvboard_update();
-  contextp->timeInc(1);
-  tfp->dump(contextp->time());
-}
+// void sim_exit(){
+//   step_and_dump_wave();
+//   tfp->close();
+//   nvboard_quit();
+// }
 
+// int main() {
+//   nvboard_bind_all_pins(top);
+//   sim_init();
 
-void sim_init(){
-  contextp = new VerilatedContext;
-  tfp = new VerilatedVcdC;
-  top = new Vtop;
-  nvboard_init();
-  contextp->traceEverOn(true);
-  top->trace(tfp, 0);
-  tfp->open("dump.vcd");
-}
+//   top->x0=0; top->x1=1; top->x2=2; top->x3=3;
+//                       top->y=0;  step_and_dump_wave();
+//                       top->y=1;  step_and_dump_wave();
+//                       top->y=2;  step_and_dump_wave();
+//                       top->y=3;  step_and_dump_wave();
 
-void sim_exit(){
-  step_and_dump_wave();
-  tfp->close();
-  nvboard_quit();
-}
+//   top->x0=3; top->x1=2; top->x2=1; top->x3=0;
+//                       top->y=0;  step_and_dump_wave();
+//                       top->y=1;  step_and_dump_wave();
+//                       top->y=2;  step_and_dump_wave();
+//                       top->y=3;  step_and_dump_wave();
 
-int main() {
-  nvboard_bind_all_pins(top);
-  sim_init();
-
-  top->x0=0; top->x1=1; top->x2=2; top->x3=3;
-                      top->y=0;  step_and_dump_wave();
-                      top->y=1;  step_and_dump_wave();
-                      top->y=2;  step_and_dump_wave();
-                      top->y=3;  step_and_dump_wave();
-
-  top->x0=3; top->x1=2; top->x2=1; top->x3=0;
-                      top->y=0;  step_and_dump_wave();
-                      top->y=1;  step_and_dump_wave();
-                      top->y=2;  step_and_dump_wave();
-                      top->y=3;  step_and_dump_wave();
-
-  sim_exit();
-}
+//   sim_exit();
+// }
